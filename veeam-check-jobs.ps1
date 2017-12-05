dc8a029f3c043fdf99bcef712111aebec26a131b
Markus Fischbacher <fischbacher.markus@gmail.com>
2017-12-05 16:04:15 +0100

    Added features

--- veeam-check-jobs.ps1
+++ veeam-check-jobs.ps1
@@ -1,5 +1,15 @@
 Add-PSSnapin VeeamPSSnapin
 
+$jobMaxDurationMinutes = @{
+    0 = 900; #Sunday
+    1 = 240; 
+    2 = 240;
+    3 = 240;
+    4 = 240;
+    5 = 240;
+    6 = 900; #Saturday
+}
+
 $jobs = Get-VBRJob
 
 $jobs | ForEach-Object {
@@ -8,46 +18,55 @@ $jobs | ForEach-Object {
 
     ## WillBeRetries not Error
     ## IsManuallyStopped is Warning
+    ## MaxDuration on Day 0-6
+
+    $lineStatus = 0
 
     if( $lastSession.IsCompleted -eq $true ) {
-        $lineStatus = 0
+        $lineStatus = [math]::Max(0, $lineStatus)
         $lineNote = "State: {0}" -f $lastSession.State
         $duration = $lastSession.EndTime - $lastSession.CreationTime
-        $lineNote = "{0} - {1}" -f $lineNote, ( "Duration {0:hh}:{0:mm}:{0:ss}" -f $duration )  
-             
+
         $linePerf = "BaseProgress=0"
-        $linePerf += " Duration=0"
-        $linePerf += " Duration={0}" -f $duration.TotalSeconds
-        $linePerf += " BackupSize={0}" -f $lastSession.BackupStats.BackupSize
-        $linePerf += " DataSize={0}" -f $lastSession.BackupStats.DataSize
-        $linePerf += " DedupRatio={0}" -f $lastSession.BackupStats.DedupRatio
-        $linePerf += " CompressRatio={0}" -f $lastSession.BackupStats.CompressRatio
+        $linePerf += "|Duration=0"
+        $linePerf += "|Duration={0}" -f $duration.TotalSeconds
+        $linePerf += "|BackupSize={0}" -f $lastSession.BackupStats.BackupSize
+        $linePerf += "|DataSize={0}" -f $lastSession.BackupStats.DataSize
+        $linePerf += "|DedupRatio={0}" -f $lastSession.BackupStats.DedupRatio
+        $linePerf += "|CompressRatio={0}" -f $lastSession.BackupStats.CompressRatio
     } else {
-        $lineStatus = 0
-        $lineNote = "State: {0} @ {1}%" -f $lastSession.State, $lastSession.BaseProgress
+        $lineStatus = [math]::Max(0, $lineStatus)
+        $lineNote = "State: {0} @ {1} &#37;" -f $lastSession.State, $lastSession.BaseProgress
         $duration = (Get-Date) - $lastSession.CreationTime
-        $lineNote = "{0} - {1}" -f $lineNote, ( "Duration {0:hh}:{0:mm}:{0:ss}" -f $duration )
         
         $linePerf = "BaseProgress={0}" -f $lastSession.BaseProgress
-        $linePerf += " BackupSize=0"
-        $linePerf += " DataSize=0"
-        $linePerf += " DedupRatio=0"
-        $linePerf += " CompressRatio=0"
+        $linePerf += "|BackupSize=0"
+        $linePerf += "|DataSize=0"
+        $linePerf += "|DedupRatio=0"
+        $linePerf += "|CompressRatio=0"
+    }
+
+    if( $duration.TotalMinutes -gt $jobMaxDurationMinutes[[int]((get-date).DayOfWeek)] ) {
+        $lineStatus = [math]::Max(1, $lineStatus)
+        $lineNote = "{0} - {1}" -f $lineNote, ( "Duration {0:hh}:{0:mm}:{0:ss} (!)" -f $duration )  
+    } else {
+        $lineNote = "{0} - {1}" -f $lineNote, ( "Duration {0:hh}:{0:mm}:{0:ss}" -f $duration )  
     }
 
     $lineNote = $lastSession.Result.ToString() + " - " + $lineNote
     if( $lastSession.Result -ne "Success" ) {
         if( $lastSession.Result -eq "Failed" ) {
-            $lineStatus = 2
+            $lineStatus = [math]::Max(2, $lineStatus)
         }
         elseif( $lastSession.Result -eq "Warning" ) {
-            $lineStatus = 1
+            $lineStatus = [math]::Max(1, $lineStatus)
         }
     } else {
-        $lineStatus = 0
-    }    
+        $lineStatus = [math]::Max(0, $lineStatus)
+    }
+
     
-    $lineId     = "veeam_job_" + $currentJob.Name
+    $lineId     = "veeam_job-" + $currentJob.Name
     $linePerf   = "{0}" -f $linePerf
     $lineNote = "{0} - see Details..." -f $lineNote
 
